10e19812a668be23171a4bf6b66cfcdc58e6f486
Khamzaev <khamzaew.j@gmail.com>
2025-02-28 21:29:01 +0300

    api

---
+++ yatube_api/api/views.py
@@ -0,0 +1,69 @@
+from rest_framework import viewsets, permissions
+from rest_framework.exceptions import PermissionDenied
+
+from posts.models import Post, Comment, Group
+from .serializers import PostSerializer, GroupSerializer, CommentSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    Набор представлений для просмотра и
+    редактирования экземпляров постов.
+    """
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def perform_update(self, serializer):
+        if serializer.instance.author != self.request.user:
+            raise PermissionDenied('Изменение чужого контента запрещено!')
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        if instance.author != self.request.user:
+            raise PermissionDenied('Удаление чужого контента запрещено!')
+        instance.delete()
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    """Набор представлений для просмотра экземпляров групп."""
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """
+    Набор представлений для просмотра и
+    редактирования экземпляров комментариев.
+    """
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        post_id = self.kwargs['post_id']
+        if not self.request.user.is_authenticated:
+            self.permission_denied(
+                self.request,
+                message='Учетные данные для аутентификации не были предоставлены.'
+            )
+        return Comment.objects.filter(post_id=post_id)
+
+    def perform_create(self, serializer):
+        serializer.save(
+            author=self.request.user,
+            post_id=self.kwargs['post_id']
+        )
+
+    def perform_update(self, serializer):
+        if serializer.instance.author != self.request.user:
+            raise PermissionDenied('Изменение чужого контента запрещено!')
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        if instance.author != self.request.user:
+            raise PermissionDenied('Удаление чужого контента запрещено!')
+        instance.delete()
